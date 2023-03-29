3a3d75e6faf93677d97b95bc35adc3e88ede42d9
mozhi21 <mozhi21@gmail.com>
2023-03-29 15:46:21 -0700

    finishing assignment 8

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// const TUITS_API = 'https://tuiter-node-server-app-otcb.onrender.com/api/tuits';
+// const API_BASE = 'https://tuiter-node-server-app-otcb.onrender.com/api';
 const API_BASE = process.env.REACT_APP_API_BASE;
 // const API_BASE = "http://localhost:4000/api";
 const TUITS_API = API_BASE +'/tuits';
